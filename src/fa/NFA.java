14e0a5e3509ff63e2e50f9ed42a9b952c01710f8
taotao <zouzhitao_cq@163.com>
2018-05-13 15:05:42 +0800

    add NFA class

--- src/fa/NFA.java
+++ src/fa/NFA.java
@@ -1,14 +1,18 @@
 package fa;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+import fa.NFA.NfaNode;
+
 
 public class NFA {
-	class NfaNode extends FaNode{
-		HashMap<Character, HashSet<NfaNode>> next;
-		
+	static class NfaNode extends FaNode{
+		private HashMap<Character, HashSet<NfaNode>> next;
+		private HashSet<NfaNode> epSet;
 		public NfaNode() {
 			super();
 			next = new HashMap<>();
@@ -25,14 +29,54 @@ public class NFA {
 				next.put(c, tmp);
 			}
 		}
+		public HashSet<NfaNode> getNext(Character c) {
+			return next.get(c);
+		}
+		public Set<Character> keySet() {
+			return next.keySet();
+		}
+		public Set<Map.Entry<Character, HashSet<NfaNode>>>  edgeEntry() {
+			return next.entrySet();
+		}
+		/*
+		 * judge if nfanode can transform through given char @{c}
+		 */
+		public boolean canTrans(char c) {
+			return next.containsKey(c);
+		}
+		/*
+		 * eps_closure(s) = delta(s,eps) UNION eps(delta(e,eps))
+		 */
+		private HashSet<NfaNode> epsClosure() {
+			HashSet<NfaNode> ret = new HashSet<>();
+			LinkedList<NfaNode> Q = new LinkedList<>();
+			Q.add(this);
+			while (!Q.isEmpty()) {
+				NfaNode now = Q.removeFirst();
+				if(now == null|| !now.canTrans(' '))continue;
+				for(NfaNode no: now.getNext(' ')){
+					if(no!=null && ret.add(no))Q.add(no);
+				}
+			}
+			return ret;
+		}
+		/*
+		 * cache eps_closure
+		 */
+		public HashSet<NfaNode> memepsClosure() {
+			if(epSet==null)epSet = epsClosure();
+			return epSet;
+		}
+		public void updateEpset() {
+			epSet = null;
+		}
 	}
 	
-	public NfaNode start,end;
-	
+	public NfaNode start,end; 
 	public NFA(NfaNode start, NfaNode end) {
 		super();
 		this.start = start;
-		this.end = end;
+		this.end = end; 
 	}
 	
 	public NFA() {
@@ -79,12 +123,49 @@ public class NFA {
 		other.end.addNextNode(ed);
 		return new NFA(st,ed);
 	}
-	public static void main(String[] args) {
-		NFA nfa = new NFA();
-		NfaNode st = nfa.start;
-		for(Character eCharacter : st.next.keySet()) {
-			System.out.println(st.next.get(eCharacter));
+	
+	public HashSet<NfaNode> travelNodes() {
+		HashSet<NfaNode> ret = new HashSet<>();
+		LinkedList<NfaNode> Q = new LinkedList<>();
+		ret.add(start);
+		Q.add(start);
+		while (!Q.isEmpty()) {
+			NfaNode now = Q.removeFirst();
+			for(HashSet<NfaNode> nodes: now.next.values()){
+				for(NfaNode no: nodes)
+					if(ret.add(no))Q.add(no);
+			}
 		}
-		System.out.println(nfa.end);
+		return ret;
 	}
+	
+	public Graph toGraph() {
+		Graph G = new Graph();
+		int num =0;
+		HashMap<NfaNode, Integer> label = new HashMap<>();
+		HashSet<NfaNode> vis = new  HashSet<>();
+		LinkedList<NfaNode> Q = new LinkedList<>();
+		Q.add(start);
+		label.put(start, num++);
+		while (!Q.isEmpty()) {
+			NfaNode now = Q.removeFirst();
+			if(!vis.add(now))continue;
+			for(Character character : now.next.keySet()){
+				HashSet<NfaNode> nebor = now.next.get(character);
+				for(NfaNode node: nebor){
+					if(!label.containsKey(node))label.put(node, num++);
+					G.addEdge(label.get(now), label.get(node), character);
+					if(!vis.contains(node))Q.addLast(node);
+				}
+			}
+		}
+		G.setStart(label.get(start));
+		G.setEnd(label.get(end));
+		return G;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
